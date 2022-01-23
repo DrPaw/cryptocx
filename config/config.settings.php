12b7416337a43448533b7027623b957716c8a3f7
sumonskys <sumonskys@gmail.com>
2022-01-23 14:31:15 +0600

    first commit

---
+++ config/config.settings.php
@@ -0,0 +1,14 @@
+<?php
+    define('APP_URL', 'http://www.cryptocx.com');
+    define('APP_URL_FORCE', false);
+
+    define('FILE_PATH', '/');
+
+    define('MYSQL_HOST', 'localhost');  // MySQL Database host (localhost, 127.0.0.1, X.X.X.X, domain.tld)
+    define('MYSQL_USER', 'root');   // MySQL User (Please not use 'root', create a dedicated user with full permision user --> go doc)
+    define('MYSQL_PASSWD', ''); // MySQL Password
+    define('MYSQL_PORT', '3306');        // MySQL Port (Set empty for not specify port)
+    define('MYSQL_DATABASE', 'cryptocx');        // MySQL Database (Use the file sql.sql for create sql requirement)
+
+    define('CRYPTED_KEY', '9mcPY03iiwQJZlepqC0wrF3a90ESxgx0MWL5MacmoKCI72PjNNBsEbLNdALD');
+?>
